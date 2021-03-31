5f8c8ddf5c3c7ba86a29ce38542b1fd2473d7e6d
bgdj17 <bgdj17@gmail.com>
2021-03-31 13:37:06 -0300

    Início da Criação da RecyclerView

--- app/src/main/java/com/example/convidados/view/AllGuestsFragment.kt
+++ app/src/main/java/com/example/convidados/view/AllGuestsFragment.kt
@@ -4,29 +4,45 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.convidados.R
+import com.example.convidados.view.adapter.GuestAdapter
 import com.example.convidados.viewModel.AllGuestsViewModel
 
 class AllGuestsFragment : Fragment() {
 
   private lateinit var allGuestsViewModel: AllGuestsViewModel
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View? {
-    allGuestsViewModel =
-    ViewModelProviders.of(this).get(AllGuestsViewModel::class.java)
-    val root = inflater.inflate(R.layout.fragment_home, container, false)
-    val textView: TextView = root.findViewById(R.id.text_home)
-    allGuestsViewModel.text.observe(viewLifecycleOwner, Observer {
-      textView.text = it
-    })
+  private val mAdapter : GuestAdapter = GuestAdapter()
+
+  override fun onCreateView(inflater: LayoutInflater,   container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    allGuestsViewModel =  ViewModelProvider(this).get(AllGuestsViewModel::class.java)
+    val root = inflater.inflate(R.layout.fragment_all, container, false)
+
+    //para fazer uma Recycler tem que ter: obter uma recycler, Layout, Adapter,
+
+    val recycler = root.findViewById<RecyclerView>(R.id.recycler_all_guest)
+    recycler.layoutManager = LinearLayoutManager(context)
+    recycler.adapter = mAdapter
+
+    //fim recycler
+
+    observer()
+
+    allGuestsViewModel.load()
+
     return root
+  }
+
+  private fun observer(){
+
+    allGuestsViewModel.guestList.observe(viewLifecycleOwner, Observer {
+      mAdapter.notifyDataSetChanged()
+
+    })
   }
 }
